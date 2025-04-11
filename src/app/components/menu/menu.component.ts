ebc602424e1447a475d001338185be808abb1222
CommanderKV <kylervisser@gmail.com>
2025-04-11 15:11:27 -0400

    Added some styles for menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [ NgFor ],
+  imports: [ NgFor, NgClass ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
@@ -15,29 +16,33 @@ export class MenuComponent {
     {
       category: "Burgers",
       items: [
-        {imageUrl: "imgs/logo.png", name: "Cheeseburger", price: 10.99, description: "A classic cheeseburger with lettuce, tomato, and cheese."},
-        {imageUrl: "imgs/logo.png", name: "Bacon Burger", price: 12.99, description: "A juicy burger topped with crispy bacon."},
+        {imageUrl: "imgs/logo.png", name: "Cheeseburger", price: 10.99, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Bacon Burger", price: 12.99, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Bacon Burger", price: 12.99, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
       ]
     },
     {
       category: "Drinks",
       items: [
-        {imageUrl: "imgs/logo.png", name: "Cola", price: 1.99, description: "Refreshing cola drink."},
-        {imageUrl: "imgs/logo.png", name: "Lemonade", price: 2.49, description: "Freshly squeezed lemonade."},
+        {imageUrl: "imgs/logo.png", name: "Cola", price: 1.99, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Lemonade", price: 2.49, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Lemonade", price: 2.49, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Lemonade", price: 2.49, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Lemonade", price: 2.49, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
       ]
     },
     {
       category: "Sides",
       items: [
-        {imageUrl: "imgs/logo.png", name: "Fries", price: 3.99, description: "Crispy golden fries."},
-        {imageUrl: "imgs/logo.png", name: "Onion Rings", price: 4.49, description: "Crispy onion rings."},
+        {imageUrl: "imgs/logo.png", name: "Fries", price: 3.99, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Onion Rings", price: 4.49, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
       ]
     },
     {
       category: "Desserts",
       items: [
-        {imageUrl: "imgs/logo.png", name: "Ice Cream", price: 2.99, description: "Creamy vanilla ice cream."},
-        {imageUrl: "imgs/logo.png", name: "Brownie", price: 3.49, description: "Rich chocolate brownie."},
+        {imageUrl: "imgs/logo.png", name: "Ice Cream", price: 2.99, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {imageUrl: "imgs/logo.png", name: "Brownie", price: 3.49, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
       ]
     }
   ]

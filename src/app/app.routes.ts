81052e3a3fea85492d6337912d7dfc9c82578577
CommanderKV <kylervisser@gmail.com>
2025-04-12 17:43:48 -0400

    Removing unecessary files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,34 @@
 import { Routes } from '@angular/router';
 
-//////////////////////////
-//  Add the components  //
-//////////////////////////
-import { HomeComponent } from './components/home/home.component';
-import { OrderComponent } from './components/order/order.component';
-import { CartComponent } from './components/cart/cart.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProductOrdersComponent } from './components/product-orders/product-orders.component';
-import { ProductComponent } from './components/product/product.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AdminComponent } from './components/admin/admin.component';
 
 ////////////////////////////////////
 //  Import authorization service  //
 ////////////////////////////////////
 import { AuthGaurdService } from './services/auth-gaurd.service';
 
+
+//////////////////////////
+//  Add the components  //
+//////////////////////////
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { CreateIngredientComponent } from './components/create-ingredient/create-ingredient.component';
+
+/////////////////////////////////////////////
+//  Create the routes for the application  //
+/////////////////////////////////////////////
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "order", component: OrderComponent },
     { path: "cart", component: CartComponent },
     { path: "login", component: LoginComponent },
-    { path: "productOrder", component: ProductOrdersComponent },
-    { path: "product", component: ProductComponent },
     { path: "menu", component: MenuComponent },
     { path: "dashboard", component: DashboardComponent },
     { path: "admin", component: AdminComponent, canActivate: [AuthGaurdService] },
+    { path: "admin/invin", component: InventoryComponent, canActivate: [AuthGaurdService] },
+    { path: "admin/createIngredient", component: CreateIngredientComponent, canActivate: [AuthGaurdService] },
 ];

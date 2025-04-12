6b41ed858dc06b2418f0aea7b3567522824d3352
CommanderKV <kylervisser@gmail.com>
2025-04-11 21:32:31 -0400

    Refactor AuthService to improve role management and update user data handling

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,11 +10,12 @@ export class AuthService {
   // Get the server URL from the environment file
   private serverUrl = environment.serverUrl;
 
-  // Setup username to be shared across components
+  // Setup vars to be shared across components
   private usernameSource = new BehaviorSubject<string | undefined>(undefined);
-
-  // Other components can subscribe to this observable to get the username
   username = this.usernameSource.asObservable();
+
+  private roleSource = new BehaviorSubject<string | undefined>(undefined);
+  role = this.roleSource.asObservable();
 
   // Update the global username
   setUsername(username: string): void {
@@ -26,13 +27,50 @@ export class AuthService {
     this.usernameSource.next(undefined);
   }
 
+  // Get global role
+  getRole(): string | undefined {
+    return this.roleSource.getValue();
+  }
+
+  // Update the global role
+  setRole(role: string): void {
+    this.roleSource.next(role);
+  }
+
+  // Remove the global role on logout
+  clearRole(): void {
+    this.roleSource.next(undefined);
+  }
+
+  // Update the user data
+  updateData() {
+    // Get the user data from the server
+    return this.http.get(`${this.serverUrl}/user`, { withCredentials: true }).subscribe({
+      next: (response: any) => {
+        // If the response is successful, set the username
+        if (response && response.data.displayName) {
+          this.setUsername(response.data.displayName);
+          this.setRole(response.data.role);
+        } else {
+          this.clearUsername();
+          this.clearRole();
+        }
+      },
+      error: () => {
+        // If there is an error, clear the username
+        this.clearUsername();
+        this.clearRole();
+      }
+    });
+  }
+
   // Inject the HTTP client to make API calls
   constructor(private http: HttpClient) {}
 
   // Google login function
   googleLogin() {
-    // Call the login endpoint on the server
-    return this.http.get(`${this.serverUrl}/user/google`, { withCredentials: true });
+    // Redirect to the Google login page
+    window.location.href = `${this.serverUrl}/user/google`;
   }
 
   // Logout function

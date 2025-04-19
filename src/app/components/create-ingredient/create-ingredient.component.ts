a80fc691f7fa6f2a69d89fb4486ca3f4aff496ea
CommanderKV <kylervisser@gmail.com>
2025-04-18 21:36:09 -0400

    Updated styles and structure for editing ingredients

--- src/app/components/create-ingredient/create-ingredient.component.ts
+++ src/app/components/create-ingredient/create-ingredient.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { AdminNavComponent } from "../admin-nav/admin-nav.component";
+import { PhotoUploadComponent } from '../photo-upload/photo-upload.component';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-create-ingredient',
-  imports: [AdminNavComponent],
+  imports: [ AdminNavComponent, PhotoUploadComponent, NgIf, FormsModule ],
   templateUrl: './create-ingredient.component.html',
   styleUrl: './create-ingredient.component.css'
 })
 export class CreateIngredientComponent {
+  name: string = '';
+  description: string = '';
+  productLink: string | undefined = undefined;
+  currentStock: number = 0;
+  photo: string | undefined = undefined;
+  photoUrl: string = 'imgs/logo.png';
 
+  editing: boolean = false;
+
+
+  // Method called when editing an 
+  // ingredient and wanting to delete it
+  deleteItem() {}
+
+  // Method called when wanting to 
+  // save an ingredient
+  saveItem() {
+    let data = {
+      name: this.name,
+      description: this.description,
+      quantity: this.currentStock,
+      photo: this.photo,
+    };
+    // Check if we are editing or not
+    if (this.editing) {
+      // Update the ingredient
+    } else {
+      // Create a new ingredient
+    }
+  }
 }

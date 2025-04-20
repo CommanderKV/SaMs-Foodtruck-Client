168b5770edad319906b25acc55b8a5b959a7ccfb
CommanderKV <kylervisser@gmail.com>
2025-04-20 16:16:27 -0400

    Added visual toggle for login and signup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-login',
-  imports: [ NgIf, NavComponent ],
+  imports: [ NgIf, NavComponent, NgClass ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   // Login or signup
   login: boolean = true;
+  loginClass: string = "active";
+  signupClass: string = "";
 
   // Inject the auth service to use the login function
   constructor(private authService: AuthService) {}
+
+  // When the user wants to login
+  loginToggle() {
+    this.login = true;
+    this.loginClass = "active";
+    this.signupClass = "";
+  }
+
+  // When the user wants to signup
+  signupToggle() {
+    this.login = false;
+    this.loginClass = "";
+    this.signupClass = "active";
+  }
 
   // Google login
   google(): void {
